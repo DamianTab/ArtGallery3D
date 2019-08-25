a09217315ba4477af3ab6b8be7842577df93eb24
dmistera2 <dmistera2@gmail.com>
2019-08-25 17:53:39 +0200

    Moved main loop

---
+++ src/main/java/artgellary/ArtGallery.java
@@ -0,0 +1,12 @@
+package artgellary;
+
+import engine.Application;
+import engine.models.GameObject;
+
+public class ArtGallery  extends Application {
+    @Override
+    public GameObject getRoot() {
+        //TODO
+        return null;
+    }
+}

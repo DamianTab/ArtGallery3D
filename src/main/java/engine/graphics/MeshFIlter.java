f1b581ef2842506f265904f3d364e58e0219aede
Damian Tabaczynski <dam.tab@wp.pl>
2019-08-25 14:10:07 +0200

    Added some necessary class v2

---
+++ src/main/java/engine/graphics/MeshFIlter.java
@@ -0,0 +1,12 @@
+package engine.graphics;
+
+import engine.models.Component;
+import engine.models.GameObject;
+
+public class MeshFIlter extends Component {
+
+    public MeshFIlter(GameObject gameObject) {
+        super(gameObject);
+    }
+
+}
